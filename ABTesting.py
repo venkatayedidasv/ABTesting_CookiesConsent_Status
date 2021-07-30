5be94248b2f00fe1e6e21f01709061a049a3f2cf
Venkata Yedida <62339740+venkatayedidasv@users.noreply.github.com>
2021-07-30 18:16:15 +0200

    Update ABTesting.py

--- ABTesting.py
+++ ABTesting.py
@@ -1,5 +1,6 @@
 import time
-
+import json
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -57,9 +58,34 @@ time.sleep(5.0)
 optimizely_status()
 print("Cookies_Status: ", status_key[3] +"  "+"Optimizely_Status: "+ status_key[4])
 
+
+optout_default_se = False
+if optout_default_se != (status_key[2]=='false'):
+    print("Optimizely compliant")
+    Optimizely_compliance = '1'
+else:
+    Optimizely_compliance = '0'
+
+myHeaders = {
+  'Content-type': 'application/json',
+  'X-SF-Token': '5-28fhToYek6HehbOr2YCg'
+}
+dsrPayload={"gauge": [{'metric': "se.Optimizely_status", 'value': Optimizely_compliance}]}
+dsPayload = json.dumps(dsrPayload)
+print(dsPayload)
+
+response = requests.post('https://ingest.eu0.signalfx.com/v2/datapoint', data=dsPayload, headers=myHeaders)
+print(response)
+print("Status code: ", response.status_code)
+
+
 driver.quit()
 
 ##print("********** Driver close ***************")
+
+
+
+
 
 
         
